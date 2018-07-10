986e68569eb1fdd0c8e42416cac059099adf2d58
francisxt <francisxt@live.com>
2018-07-10 02:40:57 -0500

    Add files via upload

---
+++ SAOSinglecallRemoting/ComponenteSAOSC/ComponenteSAOSC.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponenteSAOSC
+{
+
+    /********************************
+     *  Aplicaciones Distribuidas   *
+     *  Francisco Gualli            *
+     *  Gr1                         *
+     *  Practica 05                 *
+     *  ComponenteSAOSC             *
+     /*******************************/
+    public class ComponenteSAOSC : MarshalByRefObject
+    {
+        private static int ID = 0;
+        private int id;
+
+        public ComponenteSAOSC()
+        {
+            id = System.Threading.Interlocked.Increment(ref ID);
+            Log.Imprimir("Se creo una instancia del Objeto Remoto ComponenteCAO,id={0}", id);
+        }
+
+        public string PrimeraLlamada()
+        {
+            Log.Imprimir("Se invoco a PrimeraLlamada()");
+            return string.Format("ComponenteCAO.id={0}", id);
+        }
+
+        public string SegundaLlamada()
+        {
+            Log.Imprimir("Se invoco a SegundaLlamada()");
+            return string.Format("ComponenteCAO.id={0}", id);
+        }
+    }
+}
---
+++ SAOSinglecallRemoting/ServidorSAO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using ComponenteSAOSC;
+
+/********************************
+*  Aplicaciones Distribuidas   *
+*  Francisco Gualli            *
+*  Gr1                         *
+*  Practica 05                 *
+*  ServidorSAO                 *
+/*******************************/
+//Lance otro ClienteSAO (puede ir a la carpeta Debug, buscar el ejecutable y presionar doble
+//clic). Cuál es el resultado?
+
+//Se puede ejecutar multiples clientes simultaneamente.
+namespace ServidorSAO
+{
+    
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            RemotingConfiguration.Configure("ServidorSAO.exe.config");
+            Utilidades.MostrarTodosLosDatos();
+            Log.EsperarParaTerminar("Presione ENTER para detener al servidor...");
+            
+        }
+    }
+}
